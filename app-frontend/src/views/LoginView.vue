88f77a942283572d999e104c11fdfd073dc019e3
ale <alessioberti@gmail.com>
2025-02-06 21:19:45 +0100

    fix main css

--- app-frontend/src/views/LoginView.vue
+++ app-frontend/src/views/LoginView.vue
@@ -1,52 +1,53 @@
 <template>
-  <div>
-    <form class="space-y-6" @submit.prevent="handleLogin">
-      <div class="space-y-4">
-        <div>
-          <input
-            id="email"
-            type="email"
-            class="input"
-            v-model="email"
-            required
-            placeholder="Inserisci la tua email"
-          />
-        </div>
-        <div>
-          <input
-            id="password"
-            type="password"
-            class="input"
-            v-model="password"
-            required
-            placeholder="Inserisci la tua password"
-          />
-        </div>
-      </div>
-
+  <div class="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+    <div class="w-full max-w-sm space-y-10">
       <div>
-        <button type="submit" class="button" :disabled="authStore.loading">
-          Sign in
-        </button>
+        <h2 class="text-center text-2xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
       </div>
-    </form>
 
-    <
-    <div class="flex items-center justify-between">
-      <router-link to="/register"> Registrati </router-link>
-      <router-link to="/reset-password"> Recupera Password </router-link>
+      <form class="space-y-6" @submit.prevent="handleLogin">
+        <div class="space-y-4">
+          <div>
+            <input id="email" type="text" class="input" v-model="email" required placeholder="Inserisci la tua email" />
+          </div>
+          <div>
+            <input
+              id="password"
+              type="password"
+              class="input"
+              v-model="password"
+              required
+              placeholder="Inserisci la tua password"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="button" :disabled="loading">Sign in</button>
+        </div>
+      </form>
+
+      <div class="flex items-center justify-between">
+        <router-link to="/register" class="font-semibold text-indigo-600 hover:text-indigo-500">
+          Registrati
+        </router-link>
+        <router-link to="/reset-password" class="font-semibold text-indigo-600 hover:text-indigo-500">
+          Recupera Password
+        </router-link>
+      </div>
+
+      <p v-if="errorMessage" class="text-red-600 text-sm">{{ errorMessage }}</p>
     </div>
   </div>
-  <p v-if="errorMessage" class="text-red-600 text-sm">{{ errorMessage }}</p>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { useRouter } from "vue-router";
-import { useAuthStore } from "../stores/auth";
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '../stores/auth';
 
-const email = ref("");
-const password = ref("");
+const email = ref('');
+const password = ref('');
 const errorMessage = ref(null);
 
 const authStore = useAuthStore();
@@ -57,9 +58,9 @@ const handleLogin = async () => {
   try {
     await authStore.login(email.value, password.value);
     // Dopo il login reindirizza alla home
-    router.push({ name: "home" });
+    router.push({ name: 'home' });
   } catch (err) {
-    errorMessage.value = authStore.error || "Errore di login";
+    errorMessage.value = authStore.error || 'Errore di login';
   }
 };
 </script>
